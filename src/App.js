e343b6e9c4e899f27b13d6dfe3890767817c7bb5
Jianyu Xiong <jianyuxiong1998@gmail.com>
2021-03-04 22:14:15 +0800

    fixed the conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css'
 // import { HashRouter as Router } from 'react-router-dom'
-<<<<<<< HEAD
-import { Table7 } from './components/Tables/Table7'
-=======
 import { Table20 } from './components/Tables/Table20'
->>>>>>> e0c00c5a9585a234210230ea7ec3889aee899ca6
 
 function App() {
   return (
@@ -12,11 +8,7 @@ function App() {
       {/* <Router>
         <Home />
       </Router> */}
-<<<<<<< HEAD
-      <Table7 /> 
-=======
       <Table20 /> 
->>>>>>> e0c00c5a9585a234210230ea7ec3889aee899ca6
     </div>
   )
 }
