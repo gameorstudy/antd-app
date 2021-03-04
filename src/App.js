e0c00c5a9585a234210230ea7ec3889aee899ca6
rasmusxiong <jianyuxiong1998@gmail.com>
2021-03-04 15:43:01 +0800

    fixed the conflix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css'
 // import { HashRouter as Router } from 'react-router-dom'
-<<<<<<< HEAD
 import { Table20 } from './components/Tables/Table20'
-=======
-import { Statistics5 } from './components/Statisticses/Statistics5'
->>>>>>> 751ddf3117f1c6f0aad5ef1adf7452cac3baada7
 
 function App() {
   return (
@@ -12,11 +8,7 @@ function App() {
       {/* <Router>
         <Home />
       </Router> */}
-<<<<<<< HEAD
       <Table20 /> 
-=======
-      <Statistics5 /> 
->>>>>>> 751ddf3117f1c6f0aad5ef1adf7452cac3baada7
     </div>
   )
 }
